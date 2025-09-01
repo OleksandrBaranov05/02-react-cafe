1d07f02421b460c75e59dc82779f7cfe5977c966
Oleksandr Baranov <baranov.sanyok1995@gmail.com>
2025-09-01 23:53:50 +0200

    react-cafe-styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App/App'
-
-// normalize + глобальні дрібні стилі
 import 'modern-normalize'
 import './index.css'
 
